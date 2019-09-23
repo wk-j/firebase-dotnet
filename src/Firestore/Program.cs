dc13b23dc3b391c787e912b85c596b54dbf7b6b4
wk-j <somnuk.wk@gmail.com>
2019-09-23 14:46:00 +0700

    Firestore

---
+++ src/Firestore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Google.Cloud.Firestore;
+
+namespace Firestore {
+    class Program {
+        static async System.Threading.Tasks.Task Main(string[] args) {
+            var dir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{dir}/.google/issue-tagging-4711c12ef09a.json");
+
+            var project = "github-issue-bot";
+            var db = FirestoreDb.Create(project);
+
+            var snapshot = await db.Collection("issues").GetSnapshotAsync();
+            foreach (var item in snapshot.Documents) {
+                Console.WriteLine(item.Id);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
