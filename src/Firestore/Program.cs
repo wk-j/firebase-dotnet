de3cfebc2e994f254d28e28cd5a32a4b68d569c4
wk-j <somnuk.wk@gmail.com>
2019-09-23 15:10:07 +0700

    Update key

--- src/Firestore/Program.cs
+++ src/Firestore/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Google.Cloud.Firestore;
 
 namespace Firestore {
     class Program {
-        static async System.Threading.Tasks.Task Main(string[] args) {
+
+        static async Task Query(FirestoreDb db) {
+            var snapshot = await db.Collection("issues").GetSnapshotAsync();
+            foreach (var item in snapshot.Documents) {
+                Console.WriteLine(item.GetValue<string>("title"));
+            }
+        }
+
+        static async Task Main(string[] args) {
             var dir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{dir}/.google/issue-tagging-4711c12ef09a.json");
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{dir}/.google/github-issue-bot-322a4456bfdf.json");
 
             var project = "github-issue-bot";
             var db = FirestoreDb.Create(project);
 
-            var snapshot = await db.Collection("issues").GetSnapshotAsync();
-            foreach (var item in snapshot.Documents) {
-                Console.WriteLine(item.Id);
-            }
+            await Query(db);
 
             Console.ReadLine();
         }
